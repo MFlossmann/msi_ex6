36966b40a8f993a514d8f815c89619a3bec0857c
Michael Floßmann <michael.floszmann@gmail.com>
2016-12-06 11:51:07 +0100

    Created the import data function.

---
+++ main.m
@@ -0,0 +1,7 @@
+%MSI Ex 6
+
+%Hendrik Vloet, Mn.: 4324249
+%Michael Floßmann 4348852
+%Stephan Schraivogel, Mn.: 4318010
+
+data = import_data('produkt_wind_Terminwerte_19550101_20151231_01346.txt')
