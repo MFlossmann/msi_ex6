86682a3a2af7c67fd7142ddd1bc1df908cd23d8d
Schraivogel <Schraivogel@users.noreply.github.com>
2016-12-08 10:33:54 +0100

    Added scaling to weibull plot

--- main.m
+++ main.m
@@ -4,13 +4,16 @@
 %Michael Floßmann 4348852
 %Stephan Schraivogel, Mn.: 4318010
 
+close all
+
+if(~wind_speed)
 wind_speed = import_data('produkt_wind_Terminwerte_19550101_20151231_01346.txt');
 
 wind_speed= wind_speed(wind_speed~= -999);
-
+end
 
 figure(1)
-histogram(wind_speed);
+h= histogram(wind_speed);
 title('Wind speed histogram')
 lx= xlabel('$\frac{m}{s}$');
 set(lx,'Interpreter','Latex');
@@ -28,4 +31,5 @@ x =fmincon(obj,x0,[],[],[],[],[],[],@confun,options);
 %Hier stimmt was nicht, und zwar gewaltig. Alles Blut unterlaufen! :D
 weibull_fit= @(v) (x(2)/x(1)).*(v./x(1)).^(x(2)-1).*exp(-(v./x(1)).^x(2));
 hold on;
-plot(linspace(0,30,100), (weibull_fit(linspace(0,30,100)))*8479)
+plot(linspace(0,30,100), (weibull_fit(linspace(0,30,100)))*length(wind_speed))
+legend('Histogram', 'Weibull fit')
